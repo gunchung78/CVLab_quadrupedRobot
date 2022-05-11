18ddfe4be2933853c789ecb3392727289941ed73
geon <gunchung78@gmail.com>
2022-05-11 12:27:08 +0900

    + test

---
+++ robot/realbot/example_automatic_gait.py
@@ -0,0 +1,128 @@
+
+from os import system, name 
+import sys
+sys.path.append("..")
+import numpy as np
+import time
+import math
+import datetime as dt
+
+import pybullet as p
+from .spotmicroai import Robot
+from .kinematicMotion import KinematicMotion,TrottingGait
+import serial
+from .serial2servo import serial_servo
+
+
+try:
+    PORT = 'COM6' #포트이름
+    BaudRate = 115200 #전송속도
+    ard = serial.Serial(PORT,BaudRate)
+    uart_bool = True
+except:
+    uart_bool = False
+    pass
+
+rtime=time.time()
+
+
+def reset():
+    global rtime
+    rtime=time.time()    
+
+def resetPose():
+    # TODO: globals are bad
+    global joy_x, joy_z, joy_y, joy_rz,joy_z
+    joy_x, joy_y, joy_z, joy_rz = 128, 128, 128, 128
+
+def consoleClear():
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
+def settingimu(imux, imuy, iXf, spurWidth): ###
+    timux = math.tan(imux)
+    timuy = math.tan(imuy)
+    rlegx = timux*spurWidth
+    llegx = -timux*spurWidth
+    flegy = timuy*(iXf+50)
+    blegy = -timuy*50
+    return rlegx, llegx, flegy, blegy
+
+robot=Robot(False,False,reset)
+
+spurWidth=robot.W/2+20
+stepLength=0
+stepHeight=72
+iXf=120
+iXb=-132
+# IDspurWidth = p.addUserDebugParameter("spur width", 0, robot.W, spurWidth)
+# IDstepHeight = p.addUserDebugParameter("step height", 0, 150, stepHeight)
+# IDheight = p.addUserDebugParameter("height", -40, 90, 40)
+
+Lp = np.array([[iXf, -100,spurWidth, 1], [iXf, -100, -spurWidth, 1],
+[-50, -100, spurWidth, 1], [-50, -100, -spurWidth, 1]])
+
+resetPose()
+trotting=TrottingGait()
+
+def main(id, command_status):
+    s=False
+    while True:
+        # bodyPos=robot.getPos()
+        # bodyOrn,_,angularVel=robot.getIMU()
+        # xr,yr,_= p.getEulerFromQuaternion(bodyOrn)
+        # distance=math.sqrt(bodyPos[0]**2+bodyPos[1]**2)
+        # if distance>50:
+        #     robot.resetBody()
+   
+        # ir=xr/(math.pi/180)
+        d=time.time()-rtime
+        # height = p.readUserDebugParameter(IDheight)
+
+        result_dict = command_status.get()
+        print(result_dict)
+        command_status.put(result_dict)
+
+        print(robot.getAngle()) ###
+        # print(sys.getsizeof(robot.getAngle())) ###
+        # rlegx, llegx, flegy, blegy= settingimu(xr, yr, iXf, spurWidth) ###
+        
+        if result_dict['StartStepping']:
+            robot.feetPosition(trotting.positions(d-3, result_dict, ))
+        else:
+            # Lp = np.array([[iXf, -100 + llegx + flegy, spurWidth, 1], 
+            #             [iXf, -100 + rlegx + flegy, -spurWidth, 1],
+            #             [-50, -100 + llegx + blegy, spurWidth, 1], 
+            #             [-50, -100 + rlegx + blegy, -spurWidth, 1]])
+                        ###
+            robot.feetPosition(Lp)
+        #roll=-xr
+
+        # roll=0
+        # robot.bodyRotation((roll,math.pi/180*((joy_x)-128)/3,-(1/256*joy_y-0.5)))
+        # bodyX=50+yr*10
+        # robot.bodyPosition((bodyX, 40+height, -ir))
+        robot.step()
+        
+        #pyserial part
+        ser = serial_servo(180, 1)
+        allangle =  robot.getAngle()*(180/math.pi)
+        if uart_bool:
+             for i in 3:
+                 for j in 2:
+                    angl_float = 90 + allangle[i][j]
+                    angle_byte = ser.angle2byte(1, angl_float)
+                    ard.write(angle_byte[0])
+                    ard.write(angle_byte[1]) 
+        else:
+            print("uart: x")
+        
+
+        consoleClear()
+
+
--- robot/run_realbot.py
+++ robot/run_realbot.py
@@ -1,13 +1,24 @@
-"""
-SpotMicroAI - RealBot 
-This is the main code for the physical Robot
-"""
+from realbot import example_automatic_gait
 
-import numpy as np
-import time
-import math
-import core.kinematics as ck
-import realbot.servos as sr
+#Road Balance 키보드 제어
+from multiprocessing import Process
+from simulation.multiprocess_kb import KeyInterrupt
 
-kinematics = ck.Kinematics()
-s1=sr.Servo(1,1)
+if __name__ == "__main__":
+    try:
+        # Keyboard input Process
+        KeyInputs = KeyInterrupt()
+        KeyProcess = Process(target=KeyInputs.keyInterrupt, args=(1, KeyInputs.key_status, KeyInputs.command_status))
+        KeyProcess.start()
+
+        # Main Process 
+        example_automatic_gait.main(2, KeyInputs.command_status)
+        
+        print("terminate KeyBoard Input process")
+        if KeyProcess.is_alive():
+            KeyProcess.terminate()
+
+    except Exception as e:
+        print(e)
+    finally:
+        print("Done... :)")
